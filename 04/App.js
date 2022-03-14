25664239ebddb9257d2dd0b75447dbb295fda386
che_gayhub <che_gayhub@outlook.com>
2022-03-14 21:56:49 +0800

    axios 以及代理配置

---
+++ 04/App.js
@@ -0,0 +1,12 @@
+
+import './APP.css'
+import React, { Component } from 'react'
+import TodoList from './components/TodoList'
+
+export default class App extends Component {
+  render() {
+    return (
+      <TodoList />
+    )
+  }
+}
--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,12 +1,29 @@
 
 import './APP.css'
 import React, { Component } from 'react'
-import TodoList from './components/TodoList'
+import axios from 'axios'
 
 export default class App extends Component {
   render() {
     return (
-      <TodoList />
+      <div>
+        <button onClick={this.getStudentInfo}> Student Click </button>
+        <button onClick={this.getStudentInfo}> Cars Click </button>
+      </div>
     )
+  }
+  getStudentInfo = () => {
+    axios.get('http://localhost:3000/runner/students').then(res => {
+      console.log('success', res)
+    }, err => {
+      console.log('error', err)
+    })
+  }
+  getCarInfo = () => {
+    axios.get('http://localhost:3000/api/cars').then(res => {
+      console.log('success', res)
+    }, err => {
+      console.log('error', err)
+    })
   }
 }
