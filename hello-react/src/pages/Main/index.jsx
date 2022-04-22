714ab103c10741c0bc9ba87fbd5fe26ad6e19869
che_gayhub <che_gayhub@outlook.com>
2022-04-22 22:46:10 +0800

    Switch组件 单匹配，路由匹配后立即结束匹配

--- hello-react/src/pages/Main/index.jsx
+++ hello-react/src/pages/Main/index.jsx
@@ -6,6 +6,7 @@ import PageHeader from '../../components/Pageheader';
 import Home from '../Home';
 import About from '../About';
 import MyLink from "../../components/MyLink";
+import { Switch } from "react-router-dom";
 
 export default class Rpage extends Component {
   render() {
@@ -19,12 +20,14 @@ export default class Rpage extends Component {
               <a className="list-group-item" href="./home.html">Home</a> */}
               {/* <NavLink activeStyle={{"background": "lightblue"}} to="/about" className="list-group-item">About</NavLink>
               <NavLink activeClassName="active" to="/home" className="list-group-item">Home</NavLink> */}
-              <MyLink to="/about">About</MyLink>
-              <MyLink to="/home">Home</MyLink>
+              <MyLink to="/a/about">About</MyLink>
+              <MyLink to="/a/home">Home</MyLink>
             </div>
           </div>
-          <Route path="/about" component={About} />
-          <Route path="/home" component={Home} />
+          <Switch>
+            <Route path="/a/about" component={About} />
+            <Route path="/a/home" component={Home} />
+          </Switch>
         </div>
       </div>
     )
